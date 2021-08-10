3df2aaa8826ef658b028fefa578205095d048eab
CoolichWithYou <Coolich.ya@gmail.com>
2021-08-10 17:48:53 +0300

    added bottom navigation bar
    it is not clickable yet

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,29 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('reword'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.phone_android_outlined), title: Text('A')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.data_saver_off_outlined), title: Text('B')),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text('C'))
+        ],
       ),
     );
   }
