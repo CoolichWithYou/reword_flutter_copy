bcecf89daaf5626d86bf05f71f590ccb57e08322
CoolichWithYou <Coolich.ya@gmail.com>
2021-08-10 18:50:24 +0300

    made navigation bar clickable
    also added titles and set decoration

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('reword'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              're',
+              style: TextStyle(color: Colors.blue),
+            ),
+            Text(
+              'word',
+              style: TextStyle(color: Colors.black),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.phone_android_outlined), title: Text('A')),
+              icon: Icon(Icons.phone_android_outlined),
+              title: Text(
+                'Learn',
+                style: TextStyle(fontSize: 14),
+              )),
           BottomNavigationBarItem(
-              icon: Icon(Icons.data_saver_off_outlined), title: Text('B')),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text('C'))
+              icon: Icon(Icons.data_saver_off_outlined),
+              title: Text(
+                'Vocabulary',
+                style: TextStyle(fontSize: 14),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text(
+                'Menu',
+                style: TextStyle(fontSize: 14),
+              ))
         ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
